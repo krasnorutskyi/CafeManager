7719c1dd8ef863dcd1fed1c5b1a0adbc5d2aaeb2
vitaliy Krasnorutskyi <vitaliikr@icloud.com>
2022-12-21 21:44:04 +0100

    Added paging

--- CafeManager.Application/IRepositories/IGenericRepository.cs
+++ CafeManager.Application/IRepositories/IGenericRepository.cs
@@ -7,6 +7,5 @@ public interface IGenericRepository<TEntity> where TEntity : EntityBase
     Task AddAsync(TEntity entity);
     Task UpdateAsync(TEntity entity);
     Task DeleteAsync(TEntity entity);
-    Task UpdateRangeAsync(IEnumerable<TEntity> entities);
     Task<TEntity> GetOneAsync(int id);
 }
---
+++ CafeManager.Application/Paging/PageParameters.cs
@@ -0,0 +1,7 @@
+namespace CafeManager.Application.Paging;
+
+public class PageParameters
+{
+    public int PageNumber { get; set; } = 1;
+    public int PageSize { get; set; } = 20;
+}
