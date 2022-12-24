1ae7009dec26b94192d97f7608fbdfdc05d33ebf
vitaliy Krasnorutskyi <vitaliikr@icloud.com>
2022-12-24 15:19:08 +0100

    implemented Dependency Injection

---
+++ CafeManager.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,25 @@
+using CafeManager.Application.IRepositories;
+using CafeManager.Infrastructure.EF;
+using CafeManager.Infrastructure.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+
+namespace CafeManager.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    {
+        var connectionString = @"Server=127.0.0.1,1401;Database=Cafe;User Id=SA;Password=12345678;TrustServerCertificate=true;";
+        services.AddDbContext<ApplicationContext>(options => options.UseSqlServer(connectionString));
+        
+        services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+        
+        return services;
+    }
+    
+    public static IServiceCollection AddServices(this IServiceCollection services)
+    {
+        return services;
+    }
+}
--- CafeManager.Infrastructure/EF/ApplicationContext.cs
+++ CafeManager.Infrastructure/EF/ApplicationContext.cs
@@ -12,7 +12,6 @@ public class ApplicationContext : DbContext
     public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
     {
     }
-
     
 
     public DbSet<Category> Categories { get; set; }
